5a2dcc4fd80e9fd8c38f8e7d42c486fd66f80cb7
Stephen-Stanbridge <stanbridgestephen@gmail.com>
2020-03-19 00:38:28 +0100

    Tests

--- warships/conftest.py
+++ warships/conftest.py
@@ -1,10 +1,16 @@
 import pytest
 from django.contrib.auth.models import User
 
-from warships.models import Game
+from warships.models import Game, Field
 
 
 @pytest.fixture
-def create_players():
+def player_a():
     player_a = User.objects.create(username='testing_a', password="testing", email='testa@test.com')
+    return player_a
+
+
+@pytest.fixture
+def player_b():
     player_b = User.objects.create(username='testing_b', password="testing", email='testb@test.com')
+    return player_b
--- warships/tests.py
+++ warships/tests.py
@@ -5,31 +5,26 @@ from warships.models import Game, Field
 
 
 @pytest.mark.django_db
-def test_creating_players(create_players):
-    assert len(User.objects.all()) == 2
-
-
-@pytest.mark.django_db
-def test_deleting_players(create_players):
-    player = User.objects.get(username='testing_a')
-    player.delete()
+def test_creating_players(player_a):
     assert len(User.objects.all()) == 1
-    assert User.objects.get(username='testing_b')
 
 
 @pytest.mark.django_db
-def test_creating_game(create_players):
-    player_a = User.objects.get(username='testing_a')
-    player_b = User.objects.get(username='testing_b')
+def test_deleting_players(player_a):
+    assert len(User.objects.all()) == 1
+    player_a.delete()
+    assert len(User.objects.all()) == 0
+
+
+@pytest.mark.django_db
+def test_creating_game(player_a, player_b):
     Game.objects.create(player_a=player_a, player_b=player_b)
     assert len(Game.objects.all()) == 1
     assert Game.objects.get(pk=1)
 
 
 @pytest.mark.django_db
-def test_creating_field(create_players):
-    player_a = User.objects.get(username='testing_a')
-    player_b = User.objects.get(username='testing_b')
+def test_creating_field(player_a, player_b):
     game = Game.objects.create(player_a=player_a, player_b=player_b)
     Field.objects.create(game=game, owner_of_field=player_a)
     assert len(Field.objects.all()) == 1
