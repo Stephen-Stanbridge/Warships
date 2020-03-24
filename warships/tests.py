4adf3006c687a98d7b859fc743581fa5bf19f22f
Stephen-Stanbridge <stanbridgestephen@gmail.com>
2020-03-24 16:30:54 +0100

    Shooting test

--- warships/tests.py
+++ warships/tests.py
@@ -1,6 +1,7 @@
 import pytest
 from django.contrib.auth.models import User
 
+from warships.field_creator import create_table
 from warships.models import Game, Field
 
 
@@ -80,6 +81,33 @@ def test_accepting_invitation(c, player_a, player_b):
     response = c.get('/game/{}/{}/accept'.format(player_b.id, player_a.id))
     game = Game.objects.all()[0]
     assert response.status_code == 302
-    assert game.is_accepted == True
+    assert game.is_accepted
     assert game.whose_turn == player_b.id
     assert len(game.field_set.all()) == 2
+
+
+@pytest.mark.django_db
+def test_shooting(c, player_a, player_b):
+    table_a = create_table(8)
+    table_b = create_table(8)
+    table_a[2][2] = 1
+    table_a[2][3] = 1
+    table_b[2][1] = 1
+    game = Game.objects.create(player_a=player_a, player_b=player_b, is_accepted=True, whose_turn=player_a.id)
+    f1 = Field.objects.create(game=game, owner_of_field=player_a, table=table_a)
+    f2 = Field.objects.create(game=game, owner_of_field=player_b, table=table_b)
+    game.field_set.set([f1, f2])
+    assert len(Game.objects.all()) == 1
+    c.force_login(player_a)
+    c.get('/game/' + str(game.id) + '/shoot/' + str(player_a.id) + '/' + str(player_b.id) + '/3/1')
+    get_player_b_table = Field.objects.get(owner_of_field=player_b).table
+    assert get_player_b_table[3][1] == 2
+    c.logout()
+    c.force_login(player_b)
+    c.get('/game/' + str(game.id) + '/shoot/' + str(player_b.id) + '/' + str(player_a.id) + '/2/2')
+    get_player_a_table = Field.objects.get(owner_of_field=player_a).table
+    assert get_player_a_table[2][2] == 3
+    c.logout()
+    c.force_login(player_a)
+    c.get('/game/' + str(game.id) + '/shoot/' + str(player_a.id) + '/' + str(player_b.id) + '/2/1')
+    assert len(Game.objects.all()) == 0
