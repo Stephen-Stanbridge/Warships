45778f2febb85eb43f8b5371574ff6e5658838e3
Stephen-Stanbridge <stanbridgestephen@gmail.com>
2020-03-29 16:13:38 +0200

    Typing and renaming functions

--- warships/field_creator.py
+++ warships/field_creator.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def create_table(size):
+def create_table(size: int) -> list:
     table = []
     for row in range (0, size):
         table.append(row)
@@ -10,7 +10,7 @@ def create_table(size):
     return table
 
 
-def positions(start_row, start_col, orientation, length):
+def positions(start_row: int, start_col: int, orientation: int, length: int) -> list:
     result = []
     if orientation == 0:
         for i in range(length):
@@ -21,7 +21,7 @@ def positions(start_row, start_col, orientation, length):
     return result
 
 
-def check_left_side(positions, field):
+def check_left_side(positions: list, field: list) -> bool:
     for row in positions:
         check_row = row[0]
         check_col = row[1] - 1
@@ -34,7 +34,7 @@ def check_left_side(positions, field):
     return True
 
 
-def check_right_side(positions, field):
+def check_right_side(positions: list, field: list) -> bool:
     for row in positions:
         check_row = row[0]
         check_col = row[1] + 1
@@ -47,7 +47,7 @@ def check_right_side(positions, field):
     return True
 
 
-def check_up_side(positions, field):
+def check_top_side(positions: list, field: list) -> bool:
     for row in positions:
         check_row = row[0] - 1
         check_col = row[1]
@@ -60,7 +60,7 @@ def check_up_side(positions, field):
     return True
 
 
-def check_down_side(positions, field):
+def check_bottom_side(positions: list, field: list) -> bool:
     for row in positions:
         check_row = row[0] + 1
         check_col = row[1]
@@ -72,8 +72,9 @@ def check_down_side(positions, field):
             return False
     return True
 
-def check_is_outside(start_row, start_col, orientation, length, field_size):
-    if orientation == 0: #vertical
+
+def is_inside(start_row: int, start_col: int, orientation: int, length: int, field_size: int) -> bool:
+    if orientation == 0:
         last_row = start_row + length - 1
         last_col = start_col
     else:
@@ -81,17 +82,18 @@ def check_is_outside(start_row, start_col, orientation, length, field_size):
         last_col = start_col + length - 1
 
     if last_col >= field_size or last_row >= field_size:
-        return True
+        return False
 
+    return True
+
+
+def check_all_sides(positions: list, field: list) -> bool:
+    if check_bottom_side(positions, field) and check_top_side(positions, field) and check_left_side(positions, field) and check_right_side(positions, field):
+        return True
     return False
 
-def check_all_sides(positions, field):
-    if check_down_side(positions, field) and check_up_side(positions, field) and check_left_side(positions, field) and check_right_side(positions, field):
-        return True
-    return False
 
-
-def create_ship(ship_size, field, amount):
+def create_ship(ship_size: int, field: list, amount: int) -> list:
     passed = False
     counter = 1
     while not passed:
@@ -99,8 +101,7 @@ def create_ship(ship_size, field, amount):
         start_row = randint(0, len(field) - 1)
         start_col = randint(0, len(field) - 1)
         ship_position = positions(start_row, start_col, orientation, ship_size)
-        if check_is_outside(start_row, start_col, orientation, ship_size, len(field)) == False and check_all_sides(
-                ship_position, field):
+        if is_inside(start_row, start_col, orientation, ship_size, len(field)) and check_all_sides(ship_position, field):
             for pos in ship_position:
                 field[pos[0]][pos[1]] = 1
             counter += 1
@@ -112,7 +113,7 @@ def create_ship(ship_size, field, amount):
 
 
 # ARRAY OF ARRAYS [ [AMOUNT OF SHIPS, HOW LONG] ]
-def create_ships(arr, table):
+def create_ships(arr: list, table: list) -> list:
     for i in range(len(arr)):
         table = create_ship(arr[i][1], table, arr[i][0])
     return table
